818e28e9ad6d8eea04ad61e22cf6ba40bfb3b1fb
mdnobin96670 <farhanahmednobin@gmail.com>
2024-05-23 19:02:18 +0600

    basic

---
+++ First-app/my-app/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1> Hello world | Iam greeting you !</h1>;
+}
+
+export default App;
---
+++ First-app/vite-project/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>vite project</h1>;
+}
+
+export default App;
